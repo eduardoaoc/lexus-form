77543f9fd410afb46f03c28c8f29f295a1d42be8
Eduardo Augusto <edudtk7@gmail.com>
2022-06-01 12:31:24 +0200

    second-commit

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
-from importlib.resources import contents
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request
-import sqlalchemy
+from models.feedback import Feedback
 from send_email import send_mail
-import sqlite3
 
 app= Flask(__name__)
 
-db=SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///lexus-form/feedback.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False 
 
-class Feedback(db.Model):
-    __tablename__ = 'feedback'
-    id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
-    rating = db.Column(db.Integer)
-    comments = db.Column(db.Text())
 
-@app.route('/')
+@app.before_first_request   #função criada para ter a segurança de que sempre um banco de dados será inicializado
+def create_db():
+    db.create_all()
+
+
 def index():
     return render_template('index.html')
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -32,19 +26,16 @@ def submit():
         rating= request.form['rating']
         comments= request.form['comments']
         print(customer, dealer, rating, comments)  
-        #db.session.add()
-        #db.session.commit()      
-    if customer == '' or dealer =='':
-        return render_template('index.html', message='Please enter required fields')             
-    try:
-        banco= sqlite3.connect('feedback.db')
-        cursor= banco.cursor()
-        cursor.execute("INSERT INTO feedback VALUES ('"+str(customer)+"', '"+str(dealer)+"', '"+int(rating)+"','"+str(comments)+"')")
-        banco.commit()
-        banco.close()
-    except:
-        print('ERROR')
-    return render_template('success.html') 
+        if customer == '' or dealer =='':
+            return render_template('index.html', message='Please enter required fields')     
+        if db.session.query(Feedback).filter(Feedback.customer==customer).count() == 0:
+            data= Feedback(customer, dealer, rating, comments)  #dados do banco de dados no qual serar introduzido os falores
+            db.session.add(data)                                #adiciona os dados no banco de dados
+            db.session.commit()                                 #fazendo um commit para fazer a confirmação do envio do dados
+            return render_template('success.html') 
+        return render_template('index.html', message='You have alredy submitted feedback')
 
 if __name__ == '__main__':
+    from db import db
+    app.init_app(app)                #inicialização do banco de dados
     app.run(debug=True)
